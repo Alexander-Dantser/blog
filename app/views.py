eeef5dc4cfb3901a9f110e31139c74f3f4d1c016
Alexander Dantser <av@dantser.ru>
2024-12-12 23:55:19 +0300

    creating functionality for displaying multiple posts

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    posts = Post.objects.all()
+    return render(request, 'home.html', {'posts': posts})
