714b97e0e9b0afdb37bcb2169059c2d597da57da
Alexander Dantser <av@dantser.ru>
2024-12-14 02:27:59 +0300

    DZ_2024_12_09 Output of post data

--- app/views.py
+++ app/views.py
@@ -8,7 +8,8 @@ def home(request):
     return render(request, 'home.html', {'posts': posts})
 
 def post(request, pk):
-    return render(request, 'post.html', {})
-
+    post = Post.objects.get(pk=pk)
+    return render(request, 'post.html', {'post': post})
+    
     # post = Post.objects.get(id=pk)
     # return render(request, 'post.html', {'post': post})
