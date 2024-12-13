7cc47a0162e7b4e4c0b920ebacb53babab150a1f
Alexander Dantser <av@dantser.ru>
2024-12-14 01:28:40 +0300

    detail post page

--- app/views.py
+++ app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import *
+from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    posts = Post.objects.all()
+    return render(request, 'home.html', {'posts': posts})
+
+def post(request, pk):
+    return render(request, 'post.html', {})
+
+    # post = Post.objects.get(id=pk)
+    # return render(request, 'post.html', {'post': post})
